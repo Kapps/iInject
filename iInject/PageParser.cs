e11d5bd5a40c1a6c5c70573aea65f052278f2073
Ognjen Ivkovic <ivkovic@edwards.usask.ca>
2013-03-16 18:27:58 -0600

    Started on web forms.

---
+++ iInject/PageParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iInject {
+	public class PageParser {
+
+		/// <summary>
+		/// Parses the forms present in the given HTML response data.
+		/// </summary>
+		public IEnumerable<WebForm> ParseForms(string ResponseData) {
+			yield break;
+		}
+	}
+}
---
+++ iInject/WebFormControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iInject {
+	/// <summary>
+	/// Indicates a single control inside a web form.
+	/// </summary>
+	public class WebFormControl {
+		
+		/// <summary>
+		/// Gets the name of this control.
+		/// </summary>
+		public string Name { get; private set; }
+
+		/// <summary>
+		/// Gets or sets the value for this control.
+		/// </summary>
+		public string Value { get; set; }
+
+		/// <summary>
+		/// Gets the type of this control, such as 'text' or 'password'.
+		/// If no input type is specified, this defaults to text.
+		/// </summary>
+		public string Type { get; private set; }
+
+		public WebFormControl(string Name, string Type, string Value) {
+			this.Name = Name;
+			this.Type = Type;
+			this.Value = Value;
+		}
+	}
+}
