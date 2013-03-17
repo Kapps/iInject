43724c4adc36e0c9fdfe5775d3213cf8c98328a2
SquirrelKnight <ksquires11@gmail.com>
2013-03-16 17:10:54 -0700

    gitignore changed

---
+++ iInject/PagesFromFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iInject
+{
+    class PagesFromFile : IPageProvider
+    {
+
+
+        public IEnumerable<Uri> GetPagesToScan()
+        {   //reads in file to scan
+            // each Uri is separated by a newline
+            string Filename = Console.ReadLine();
+            var Lines = File.ReadAllLines(Filename);
+            foreach (var line in Lines)
+            {
+                Uri path = new Uri(line);
+                yield return path;
+            }
+        }
+    }
+}
