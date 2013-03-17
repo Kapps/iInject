0ed4caacf8125407a188cb84f2754123f8a0df0d
SquirrelKnight <ksquires11@gmail.com>
2013-03-16 21:18:16 -0700

    started new injection provider

---
+++ iInjectProviders/EqualsAndQuotes.cs
@@ -0,0 +1,39 @@
+﻿using iInject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iInjectProviders {
+	/// <summary>
+	/// a simple ' or '1'='1' -- ' attempt to access users
+	/// </summary>
+	class EqualsAndQuotes {
+		public string VulnerabilityName {
+			get { return "Sql Injection"; }
+		}
+
+		public InjectionSession Session { get; private set; }
+
+		public EqualsAndQuotes(InjectionSession Session) {
+			this.Session = Session;
+		}
+
+		public async Task<IEnumerable<VulnerabilityDetails>> ScanForVulnerabilitiesAsync(WebForm Form) {
+			List<VulnerabilityDetails> Results = new List<VulnerabilityDetails>();
+			var Parser = Session.Crawler.Parser;
+			foreach(var Control in Form.Controls) {
+				Control.Value = "' or '1'='1' -- '";
+				var SubmitTask = Form.SubmitAsync(Parser);
+				PageResponse Response = null;
+
+			}
+			return Results;
+		}
+
+		public string Name {
+			get { return "Sql Injection equals scanner"; }
+		}
+	}
+}
